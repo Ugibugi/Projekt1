6641596df5b7dbcc45cf75e9d790ffe1469ed792
Ugibugi <radek101998@interia.pl>
2018-12-15 18:41:16 +0100

    minor fixes

--- main.cpp
+++ main.cpp
@@ -50,7 +50,6 @@ int main(int argc, char* argv[])
 	Game game(Game_Window);
 	game.load();
 	SDL_Event e;
-	
 	while (!game.quit)
 	{
 		
--- uCollisionHandler.h
+++ uCollisionHandler.h
@@ -32,12 +32,12 @@ public:
 		typeMap.insert(groupId, std::type_index(typeid(T)));
 	}
 	template<typename T1,typename T2>
-	void addHandler(uint32_t groupId1, uint32_t groupId2, std::function<void(T1*,T2*)> fun)
+	void addCall(uint32_t groupId1, uint32_t groupId2, std::function<void(T1*,T2*)> fun)
 	{
-		static_assert(std::is_pointer<T1>::value, "Template type must not be a pointer!");
+		/*static_assert(std::is_pointer<T1>::value, "Template type must not be a pointer!");
 		static_assert(std::is_pointer<T2>::value, "Template type must not be a pointer!");
-		static_assert(std::type_index(typeid(T1)) == typeMap.at(groupId1), "Type 1 does not match type assigned to Group 1");
-		static_assert(std::type_index(typeid(T2)) == typeMap.at(groupId1), "Type 2 does not match type assigned to Group 2");
+		assert(std::type_index(typeid(T1)) == typeMap.at(groupId1), "Type 1 does not match type assigned to Group 1");
+		assert(std::type_index(typeid(T2)) == typeMap.at(groupId1), "Type 2 does not match type assigned to Group 2");*/
 
 		auto index = std::minmax({ groupId1,groupId2 }); // to make unordered pair of groups (A,B) = (B,A)
 		
@@ -49,6 +49,11 @@ public:
 	void call(uint32_t groupId1, uint32_t groupId2, void* arg1, void* arg2)
 	{
 		callMap.at(std::minmax({ groupId1,groupId2 }))->call(arg1, arg2);
+	}
+	bool contains(uint32_t gId1, uint32_t gId2)
+	{
+		if (callMap.count(std::minmax({ gId1,gId2 })) < 0) return true;
+		else return false;
 	}
 private:
 	std::unordered_map<uint32_t, std::type_index> typeMap;
