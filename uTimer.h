847b281c394bb55372751c4180d1a84f501a9730
Ugibugi <radek101998@interia.pl>
2019-02-13 20:14:06 +0100

    Delay call Timer added

--- uTimer.h
+++ uTimer.h
@@ -3,27 +3,15 @@
 #include <chrono>
 namespace utl
 {
-	class uSynchronisedTimer
+	class uTimer_base
 	{
 	public:
-		uSynchronisedTimer(uint32_t miliseconds)
-			:_duration(miliseconds)
+		uTimer_base()
 		{
 			_lastTime = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
-			_lastTime.time_since_epoch() = _lastTime.time_since_epoch() - (_lastTime.time_since_epoch() % miliseconds);
-
 		}
-		~uSynchronisedTimer() = default;
-		void updateTimer()
-		{
-			auto currentTime = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
-			if (std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - _lastTime) > _duration)
-			{
-				_lastTime = currentTime;
-				_timedCalls.CallAll();
-			}
+		virtual void updateTimer() = 0;
 
-		}
 		template<typename... Types>
 		void addCall(uint32_t call_id, Types... args)
 		{
@@ -33,10 +21,71 @@ namespace utl
 		{
 			_timedCalls.removeCall(call_id);
 		}
-	private:
+	protected:
 		uBindedFunctionStorage<> _timedCalls;
-		std::chrono::time_point<std::chrono::steady_clock,std::chrono::milliseconds> _lastTime;
+		std::chrono::time_point<std::chrono::steady_clock, std::chrono::milliseconds> _lastTime;
+	};
+
+
+	class uSynchronisedTimer : public uTimer_base
+	{
+	public:
+		uSynchronisedTimer(uint32_t miliseconds)
+			:_duration(miliseconds)
+		{
+			//_lastTime.time_since_epoch() = _lastTime.time_since_epoch() - (_lastTime.time_since_epoch() % miliseconds); 
+			//left side ist const so it has no effect. ( ? ) TODO examine this
+		}
+		~uSynchronisedTimer() = default;
+		virtual void updateTimer() override
+		{
+			auto currentTime = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
+			if (std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - _lastTime) > _duration)
+			{
+				_lastTime = currentTime;
+				_timedCalls.CallAll();
+			}
+
+		}
+	private:
 		std::chrono::milliseconds _duration;
 	};
 
+
+	class uDelayTimer : protected uTimer_base
+	{
+		struct DelayedCall
+		{
+			uint32_t milisecondsLeft;
+			uint32_t callId;
+		};
+	public:
+		template<typename... Types>
+		void DelayCall(uint32_t delay, Types... args)
+		{
+			_callList.push_back({ delay,_idCounter });
+			_timedCalls.addCall(_idCounter, args...);
+			_idCounter++;
+		}
+		virtual void updateTimer() override
+		{
+			auto currentTime = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
+			auto deltaTime = currentTime - _lastTime;
+
+			for (auto call = _callList.begin(); call != _callList.end(); ++call)
+			{
+				call->milisecondsLeft -= deltaTime.count();
+				if (call->milisecondsLeft <= 0)
+				{
+					_timedCalls.Call(call->callId);
+					_timedCalls.removeCall(call->callId);
+					_callList.erase(call);
+				}
+			}
+		}
+	private:
+		uint32_t _idCounter=0;
+		std::list<DelayedCall> _callList;
+		
+	};
 }
